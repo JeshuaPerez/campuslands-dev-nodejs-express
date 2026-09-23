6b56fbb783e4f1b2a704e8da070165a385db64f2
Jeshua Perez <odsmanjeshua@gmail.com>
2026-09-23 00:16:20 -0600

    feat(node): resolver ejercicio 28 intermedio - coleccion de ejemplos HTTP
    Tematica battle royale. CRUD de jugadores; requests.http trae
    ejemplos listos (caso feliz, 400, 404) para REST Client.

---
+++ intermedio/ejercicio-28/resoluciones/jeshua-perez/src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { jugadoresRouter } from "./routes/jugadores.routes.js";
+
+export const app = express();
+
+app.use(express.json());
+app.use("/jugadores", jugadoresRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ ok: false, message: "Ruta no encontrada" });
+});
